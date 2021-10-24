4ef926654c2c629c87193e56a6c043b22901fe3a
meiyokraken <loic.demesmay@gmail.com>
2021-10-24 16:09:41 +0200

    Add the less or more dices options.

--- src/ButtonDice.js
+++ src/ButtonDice.js
@@ -9,28 +9,41 @@ class ButtonDice extends React.Component {
 
         this.state = {
             active : true,
-            dices: [1,1]
+            dices: [1,1,6,6]
         }
 
         this.launchDice = this.launchDice.bind(this);
+        this.setNbDice = this.setNbDice.bind(this);
     }
 
     launchDice() {
-        let dice1 = Math.floor(Math.random() * 6) + 1;        
-        let dice2 = Math.floor(Math.random() * 6) + 1;
+        let newDices = [];
+        for(let i = 0; i < this.state.dices.length; i++) {
+            newDices.push(Math.floor(Math.random() * 6) + 1);
+        }
+        this.setState({dices: newDices});
+    }
 
-        this.setState({dices: [dice1, dice2]});
+    setNbDice(diff) {
+        let newDices = this.state.dices;
+        diff < 0 ? newDices.pop() : newDices.push(1);
+        this.setState({dices: newDices});
     }
 
     render () {
         return (
             <div className="ButtonDice">
-                <div className="Dices">                        
-                    <Dice diceFace={this.state.dices[0]}/>
-                    <Dice diceFace={this.state.dices[1]}/>
+                <div className="Dices">   
+                { 
+                    this.state.dices.map( (idx) => { return <Dice diceFace={idx}/> })
+                }                     
                 </div>
                 { this.state.active ? 
-                    <button className="btn-dice-launch" onClick={ this.launchDice }>{ this.props.text }</button> 
+                    <div  className="btn-dices-panel">
+                        <button onClick={ () => { this.setNbDice(-1) }}>Less Dices</button>
+                        <button onClick={ this.launchDice }>{ this.props.text }</button>
+                        <button onClick={ () => { this.setNbDice(1) }}>More Dice</button>
+                    </div>
                     : 
                     <p>{ this.props.desactiveText }</p> 
                 }
